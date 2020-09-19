b28a087e67d7e982b1065e0c7858f362dd5ce08a
ouyangjun <ouyangjun@zhangyue.com>
2020-09-19 15:33:46 +0800

    类型转换

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
-### tool
-Golang常见工具库
+### Golang常见工具库
 
+#### time.go文件 时间操作
 
-| 文件 | 函数 | 说明 |
-| --- | --- | --- |
-| time.go | TimeUnix() | 获取当前时间戳 |
-| time.go | DateTime() | 获取当前时间 |
-| time.go | FormatStringToTime() | 字符串转换成Time |
+| 函数 | 说明 |
+| --- | --- |
+| TimeUnix() | 获取当前时间戳 |
+| DateTime() | 获取当前时间 |
+| FormatStringToTime() | 字符串转换成Time |
 
+#### format.go文件 类型转换
 
+| 函数 | 说明 |
+| --- | --- |
+| FormatInt(data interface{}) (int, error) | 接口类型转换到整型 |
+| FormatInt64(data interface{}) (int64, error) | 接口类型转换到int64 |
+| FormatFloat(data interface{}) (float64, error) | 接口类型转换到float64 |
+| FormatString(data interface{}) (string, error) | 接口类型转换成string |
--- format.go
+++ format.go
@@ -1,10 +1,10 @@
 package tool
 
-import(
+import (
 	"encoding/json"
+	"errors"
 	"reflect"
 	"strconv"
-	"errors"
 )
 
 // FormatInt
@@ -22,6 +22,25 @@ func FormatInt(data interface{}) (int, error) {
 		return int(reflect.ValueOf(data).Uint()), nil
 	case string:
 		return strconv.Atoi(data.(string))
+	}
+	return 0, errors.New("invalid value type")
+}
+
+// FormatInt64
+// 转换成int64
+func FormatInt64(data interface{}) (int64, error) {
+	switch data.(type) {
+	case json.Number:
+		i, err := data.(json.Number).Int64()
+		return i, err
+	case float32, float64:
+		return int64(reflect.ValueOf(data).Float()), nil
+	case int, int8, int16, int32, int64:
+		return int64(reflect.ValueOf(data).Int()), nil
+	case uint, uint8, uint16, uint32, uint64:
+		return int64(reflect.ValueOf(data).Uint()), nil
+	case string:
+		return strconv.ParseInt(data.(string), 10, 64)
 	}
 	return 0, errors.New("invalid value type")
 }
@@ -67,4 +86,70 @@ func FormatString(data interface{}) (string, error) {
 		return data.(string), nil
 	}
 	return "", errors.New("invalid value type")
+}
+
+// FormatMapStringString
+// 转换为map类型其中key,value都是字符类型的
+func FormatMapStringString(data interface{}) (map[string]string, error) {
+	if mapData, ok := data.(map[string]string); ok {
+		return mapData, nil
+	} else {
+		return nil, errors.New("invalid map type")
+	}
+}
+
+// FormatMapString
+// 转换为map，key为字符串类型
+func FormatMapString(data interface{}) (map[string]interface{}, error) {
+	if mapData, ok := data.(map[string]interface{}); ok {
+		return mapData, nil
+	} else {
+		return nil, errors.New("invalid map type")
+	}
+}
+
+// FormatArrayString
+// 转成字符串数组
+func FormatArrayString(data interface{}) ([]string, error) {
+	if arrData, ok := data.([]string); ok {
+		return arrData, nil
+	} else {
+		return nil, errors.New("invalid map type")
+	}
+}
+
+// FormatArrayInterface
+// 转成字符串数组
+func FormatArrayInterface(data interface{}) ([]interface{}, error) {
+	if arrData, ok := data.([]interface{}); ok {
+		return arrData, nil
+	} else {
+		return nil, errors.New("invalid map type")
+	}
+}
+
+// FormatArrayMap
+// 转换为map的数组
+func FormatArrayMap(data interface{}) ([]map[string]interface{}, error) {
+	var tmp []interface{}
+	var tmpMap []map[string]interface{}
+
+	var ok = false
+	if tmpMap, ok = data.([]map[string]interface{}); ok {
+		return tmpMap, nil
+	}
+
+	if tmp, ok = data.([]interface{}); ok {
+		list := []map[string]interface{}{}
+		for _, valIf := range tmp {
+			var valMap map[string]interface{}
+			if valMap, ok = valIf.(map[string]interface{}); !ok {
+				return nil, errors.New("invalid array map type")
+			}
+			list = append(list, valMap)
+		}
+		return list, nil
+	}
+
+	return nil, errors.New("invalid array map type")
 }
