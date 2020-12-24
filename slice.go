6e34539af4de41e67df3c4440242a064eab211ec
oyjjpp <ouyangjuncust@aliyun.com>
2020-12-24 14:26:45 +0800

    format code

--- slice.go
+++ slice.go
@@ -3,9 +3,9 @@ package tool
 // MakeSlice
 // 创建二维切片
 func MakeIntSlice(row, column int) [][]int {
-    data := make([][]int, row)
-    for index := range data {
-        data[index] = make([]int, column)
-    }
-    return data
+	data := make([][]int, row)
+	for index := range data {
+		data[index] = make([]int, column)
+	}
+	return data
 }
