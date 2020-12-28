a70625dce0b0b3bab88432ad99e75e10d8331799
oyjjpp <ouyangjuncust@aliyun.com>
2020-12-28 14:14:21 +0800

    压缩工具函数

--- compress/compress.go
+++ compress/compress.go
@@ -1,16 +1,111 @@
-package tool
+package compress
 
 import (
+	"bytes"
+	"compress/flate"
 	"compress/gzip"
-	"strings"
+	"compress/zlib"
+	"io/ioutil"
 )
 
-// GzipData
+// FlateEncode
+// deflate压缩数据
+// @param data 待压缩的数据
+// @param level 压缩等级
+func FlateEncode(data []byte, level int) ([]byte, error) {
+	var buffer bytes.Buffer
+	flateWrite, err := flate.NewWriter(&buffer, level)
+	if err != nil {
+		return nil, err
+	}
+	// Fixed bug 为什么不能使用defer调用，Close不仅是释放资源，还有flush
+	// defer flateWrite.Close()
+	flateWrite.Write(data)
+	flateWrite.Close()
+	return buffer.Bytes(), nil
+}
+
+// FlateDecode
+// deflate解压数据
+// @param data 待解压的数据
+func FlateDecode(data []byte) ([]byte, error) {
+	content := bytes.NewBuffer(data)
+	flateReader := flate.NewReader(content)
+	return ioutil.ReadAll(flateReader)
+}
+
+// GzipEncode
 // gzip压缩数据
-func GzipData(data string) string {
-	content := &strings.Builder{}
-	gzipWrite := gzip.NewWriter(content)
-	defer gzipWrite.Close()
-	gzipWrite.Write([]byte(data))
-	return content.String()
+// @param data 待压缩的数据
+func GzipEncode(data []byte) []byte {
+	var buffer bytes.Buffer
+	gzipWriter := gzip.NewWriter(&buffer)
+	gzipWriter.Write(data)
+	gzipWriter.Close()
+	return buffer.Bytes()
+}
+
+// GzipLevelEncode
+// gzip压缩数据
+// @param data 待压缩的数据
+// @param level 压缩等级
+func GzipLevelEncode(data []byte, level int) ([]byte, error) {
+	var buffer bytes.Buffer
+	gzipWrite, err := gzip.NewWriterLevel(&buffer, level)
+	if err != nil {
+		return nil, err
+	}
+
+	gzipWrite.Write(data)
+	gzipWrite.Close()
+	return buffer.Bytes(), nil
+}
+
+// GzipDncode
+// 解压gzip压缩数据
+func GzipDncode(data []byte) ([]byte, error) {
+	content := bytes.NewReader([]byte(data))
+	gzipReader, err := gzip.NewReader(content)
+	if err != nil {
+		return nil, err
+	}
+	return ioutil.ReadAll(gzipReader)
+}
+
+// ZlibEncode
+// zlib压缩数据
+// @param data 待压缩的数据
+func ZlibEncode(data []byte) []byte {
+	var buffer bytes.Buffer
+	zlibWriter := zlib.NewWriter(&buffer)
+	zlibWriter.Write(data)
+	zlibWriter.Close()
+	return buffer.Bytes()
+}
+
+// ZlibLevelEncode
+// zlib压缩数据
+// @param data 待压缩的数据
+// @param level 压缩等级
+func ZlibLevelEncode(data []byte, level int) ([]byte, error) {
+	var buffer bytes.Buffer
+	zlibWriter, err := zlib.NewWriterLevel(&buffer, level)
+	if err != nil {
+		return nil, err
+	}
+
+	zlibWriter.Write(data)
+	zlibWriter.Close()
+	return buffer.Bytes(), nil
+}
+
+// GzipDncode
+// 解压gzlib压缩数据
+func ZlibDncode(data []byte) ([]byte, error) {
+	content := bytes.NewReader([]byte(data))
+	zlibReader, err := zlib.NewReader(content)
+	if err != nil {
+		return nil, err
+	}
+	return ioutil.ReadAll(zlibReader)
 }
--- compress/compress_test.go
+++ compress/compress_test.go
@@ -1,4 +1,4 @@
-package tool
+package compress
 
 import (
 	"reflect"
@@ -6,11 +6,85 @@ import (
 	"unsafe"
 )
 
-func TestGzipData(t *testing.T) {
-	data := "this is a mess"
-	rs := GzipData(data)
+func TestFlateData(t *testing.T) {
+	data := "this is a message 这是一个消息"
+	t.Log(len(data))
+	// 压缩数据
+	rs, err := FlateEncode([]byte(data), -1)
+	// rs, err := FlateEncodeV(data)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	t.Log(len(rs))
+
+	// 解压数据
+	res, err := FlateDecode(rs)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	t.Log(len(res), string(res))
+}
+
+func TestGzipEncode(t *testing.T) {
+	data := "this is a message"
+	rs := GzipEncode([]byte(data))
 	t.Log(len(data))
 	t.Log(len(rs))
+	if res, err := GzipDncode(rs); err == nil {
+		t.Log(string(res))
+	} else {
+		t.Error(err.Error())
+	}
+
+}
+
+func TestGzipLevelEncode(t *testing.T) {
+	data := `this is a message；239o8jv9接口路径而；a9324jva9023v速看的浪费加埃里克顺道减肥alkjfaaoi挨饿的等级
+	kj34j9fqu案例二京东方jfaaldjf阿菊发阿施蒂利克埃里克对接放埃里克地方就埃里克记得发完全二问沙发阿斯蒂芬阿斯蒂芬发
+	阿斯利康对接放曲儿GV打发va啊的说法昂防守打法(⊙o⊙)…5帅哥商定发阿斯蒂芬gas大飞哥阿达发送到阿斯顿发达案发生的发生发
+	阿萨德，放开就埃里克森2342访问发起个分ads法 aefja3q904kajflk埃里克染发剂阿里09i90jojlkjaf阿里副科级阿拉山口的`
+	rs, err := GzipLevelEncode([]byte(data), 9)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	t.Log("原数据长度:", len(data))
+	t.Log("压缩数据长度", len(rs))
+	if res, err := GzipDncode(rs); err == nil {
+		t.Log(string(res))
+	} else {
+		t.Error(err.Error())
+	}
+}
+
+func TestZlibEncode(t *testing.T) {
+	data := "this is a message"
+	rs := ZlibEncode([]byte(data))
+	t.Log(len(data))
+	t.Log(len(rs))
+	if res, err := ZlibDncode(rs); err == nil {
+		t.Log(string(res))
+	} else {
+		t.Error(err.Error())
+	}
+
+}
+
+func TestZlibLevelEncode(t *testing.T) {
+	data := `this is a message；239o8jv9接口路径而；a9324jva9023v速看的浪费加埃里克顺道减肥alkjfaaoi挨饿的等级
+	kj34j9fqu案例二京东方jfaaldjf阿菊发阿施蒂利克埃里克对接放埃里克地方就埃里克记得发完全二问沙发阿斯蒂芬阿斯蒂芬发
+	阿斯利康对接放曲儿GV打发va啊的说法昂防守打法(⊙o⊙)…5帅哥商定发阿斯蒂芬gas大飞哥阿达发送到阿斯顿发达案发生的发生发
+	阿萨德，放开就埃里克森2342访问发起个分ads法 aefja3q904kajflk埃里克染发剂阿里09i90jojlkjaf阿里副科级阿拉山口的`
+	rs, err := ZlibLevelEncode([]byte(data), 9)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	t.Log("原数据长度:", len(data))
+	t.Log("压缩数据长度", len(rs))
+	if res, err := ZlibDncode(rs); err == nil {
+		t.Log(string(res))
+	} else {
+		t.Error(err.Error())
+	}
 }
 
 func TestPtrType(t *testing.T) {
